6288b61f1bcb6fe4c4283b7b194604e2fe3e6393
E-studio <rickyurbina@gmail.com>
2020-04-13 15:54:58 -0600

    inicio

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/tigerApp';
+} else {
+urlDB = 'mongodb+srv://schoolapp:HORUgkx7i8L4qLYo@cmstudio-0lz0x.mongodb.net/tigerApp';
+
+}
+
+process.env.URLDB = urlDB;
